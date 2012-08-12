c88b446bd9fb23e0bf8c7f46f7ba5113e73d6d85
Louis Li <louisrli@gmail.com>
2012-08-11 23:49:39 -0400

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='grabrc-client',
+      version='0.1',
+      description='Lightweight, portable Github wrapper. \
+Retrieval of dotfiles (.emacs, .vimrc, etc.) in any environment',
+      author='Louis Li',
+      url='http://www.github.com/louisrli/grabrc-client/',
+      author_email='pong@outlook.com',
+      packages=['client'],
+      entry_points = {
+          'console_scripts': ['grabrc = client.client:main']
+      }
+
+    )
