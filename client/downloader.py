fb62658c55bb30790762746801c621cc9c4f4bec
Louis Li <louisrli@gmail.com>
2013-01-13 02:25:10 -0800

    Added GPLv3 license and license headers to files.

--- client/downloader.py
+++ client/downloader.py
@@ -1,3 +1,20 @@
+"""
+Functions for downloading files
+"""
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import StringIO
 import urllib2
 import zipfile
@@ -9,7 +26,6 @@ from const import Const
 import os
 import logging
 import sys
-
 
 def _get_grabrc_archive(username, tar_or_zip):
     """
